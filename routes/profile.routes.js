efd881421bfe3bab7bd443d1b3340ae84ae5c5d0
johnimeson0 <johnimeson0@gmail.com>
2022-08-08 16:02:37 +0100

    4John - create profile and edit profile update

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,13 +1,32 @@
 const router = require("express").Router();
+const User = require("../models/User.model");
 
 router.get("/profile", (req, res, next) => {
     const user = req.session.user
     res.render("profile/profile", {user});
 });
 
+router.get("/profile/:id", (req, res, next) => {
+    const {id} = req.params
+    User.findById(id).then((user) => {
+        res.render("profile/profile", user);
+    })
+    .catch(err => next(err))
+})
 
 router.get("/edit-profile", (req, res, next) => {
-    res.render("profile/edit-profile");
+    const user = req.session.user
+        res.render("profile/edit-profile", user);
 });
+
+router.post("edit-profile", (req, res, next) => {
+    
+})
+
+router.get("/create-profile", (req, res, next) => {
+    const user = req.session.user
+        res.render("profile/create-profile", user);
+});
+
 
 module.exports = router;
